cfeaa309d5479371289e956573b0c000380b58b5
jbc5099 <jbc5099@gmail.com>
2019-09-16 18:56:40 -0600

    created skeleton for routes and controllers

---
+++ server/routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router()
+
+router.get('/list', (req, res) => {
+
+})
+
+router.get('/:product_id', (req, res) => {
+
+})
+
+router.get('/:product_id/related', (req, res) => {
+
+})
+
+router.get('/:product_id/styles', (req, res) => {
+  
+})
+
+module.exports = router;
