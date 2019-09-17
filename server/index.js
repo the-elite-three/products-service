98f83287a7b66b51109819bff682c978c2dfcd3d
jbc5099 <jbc5099@gmail.com>
2019-09-16 19:02:27 -0600

    added routes to index.js

--- server/index.js
+++ server/index.js
@@ -2,4 +2,8 @@ const express = require('express');
 
 const app = express();
 
+const products = require('routes.js');
+
 app.listen(3001, () => console.log('listening on port 3001'));
+
+app.use('/products', products);
