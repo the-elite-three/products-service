6b643f3e2c7778661ae9f80ec1954d884239e056
jbc5099 <jbc5099@gmail.com>
2019-09-20 10:01:11 -0600

    added reduce function to remove duplicate data after query

--- server/controllers.js
+++ server/controllers.js
@@ -8,17 +8,50 @@ module.exports.listQuery = (res, page = 1, count = 5) => {
 }
 
 module.exports.productQuery = (param, res) => {
+  let data = null;
   db('products').where('id', param)
-    .then((query) => res.json(query))
+    .then((query) => data = query[0])
+    .then(() => db('features').where('product_id', param))
+    .then((query) => query.map((row) => {return { feature: row.feature, value: row.value }}))
+    .then((query) => data.features = query)
+    .then(() => res.json(data))
 }
 
 module.exports.stylesQuery = (param, res) => {
-  db('styles').where('productId', param)
-    .then((query) => res.json(query))
+  let data = null;
+  db('styles').where('productId', param).leftJoin('photos', 'styles.id', '=', 'photos.styleId')
+    .leftJoin('skus', 'styles.id', '=', 'skus.styleId')
+    //.then((query) => data = query)
+    //.then((query) => query.map((style) => db('photos').where('styleId', style.id)))
+    //.then((query) => Promise.all(query))
+    .then((query) => reduceStyles(query, param))
+    .then((parsed) => res.json(parsed))
 }
 
 module.exports.relatedQuery = (param, res) => {
   db('related').where('current_product_id', param)
     .then((query) => query.map((row) => row.related_product_id))
     .then((relArr) => res.json(relArr))
+}
+
+const reduceStyles = (data, id) => {
+  const parsedData = data.reduce((acc, el) => {
+    if (acc[el.styleId]) {
+      acc[el.styleId].photos.push({thumbnail_url: el.thumbnail_url, url: el.url})
+      acc[el.styleId].skus.push({[el.size]: el.id})
+    } else {
+        Object.assign(acc, {[el.styleId]: {
+          style_id: el.styleId,
+          name: el.name, 
+          sale_price: el.sale_price, 
+          origonal_price: el.origonal_price, 
+          default_style: el.default_style
+          }})
+        acc[el.styleId].photos = [{thumbnail_url: el.thumbnail_url, url: el.url}]
+        acc[el.styleId].skus = [{[el.size]: el.id}]
+      }
+      return acc;
+  }, {})
+
+  return {product_id: id, results: Object.values(parsedData)}
 }
